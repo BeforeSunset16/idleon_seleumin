312b0b6b4c117c7e5a8245f965e7c08ce3e78c4f
BeforeSunset16 <zh.zhang.apply@outlook.com>
2024-08-23 21:43:55 -0400

    add farming, plot data

--- test.ts
+++ test.ts
@@ -1,11 +1,26 @@
-export const createArrayOfArrays = (array) => {
-  return array?.map((object) => {
-    if (!Array.isArray(object)) {
-      delete object?.length;
-    }
-    return Object.values(object);
-  });
-};
+const rawFarmingPlot = tryToParse(idleonData?.FarmPlot);
+const rawFarmingRanks = tryToParse(idleonData?.FarmRank);
+const [farmingRanks, ranksProgress, upgradesLevels] = rawFarmingRanks || [];
 
-const alchemyRaw = createArrayOfArrays(idleonData?.CauldronInfo) || idleonData?.CauldronInfo;
-const liquids = alchemyRaw?.[6]
+const plot = rawFarmingPlot?.map(([seedType, progress, cropType, isLocked, cropQuantity, currentOG, cropProgress]: number[], index: number) => {
+  const type = Math.round(const_seedInfo?.[seedType]?.cropIdMin + cropType);
+  const growthReq = 14400 * Math.pow(1.5, seedType);
+  const rank = farmingRanks?.[index];
+  const rankProgress = ranksProgress?.[index];
+  const rankRequirement = (7 * rank + 25 * Math.floor(rank / 5) + 10) * Math.pow(1.11, rank);
+  return {
+    rank,
+    rankProgress,
+    rankRequirement,
+    seedType,
+    cropType: type,
+    cropQuantity,
+    cropProgress,
+    progress,
+    growthReq,
+    isLocked,
+    currentOG,
+    cropRawName: `FarmCrop${type}.png`,
+    seedRawName: `Seed_${seedType}.png`
+  }
+})
